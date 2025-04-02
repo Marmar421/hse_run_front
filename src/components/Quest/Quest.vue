6be3c3471f24963424b09f145d2c655e95883319
burlak1n <roma.s-2012@yandex.ru>
2025-04-02 03:05:49 +0300

    Изменение способа загрузки QR-кода в компоненте Quest. Теперь QR-код загружается в формате base64 из JSON-ответа вместо использования blob. Это улучшает обработку данных и упрощает отображение изображения.

--- src/components/Quest/Quest.vue
+++ src/components/Quest/Quest.vue
@@ -103,8 +103,8 @@ export default {
         const response = await fetch('/api/auth/qr')
         if (!response.ok) throw new Error('Ошибка загрузки QR-кода')
         
-        const blob = await response.blob()
-        this.qrImageSrc = URL.createObjectURL(blob)
+        const qrData = await response.json()
+        this.qrImageSrc = 'data:image/png;base64,' + qrData.qr_image
         this.qrModalVisible = true
       } catch (error) {
         alert('Ошибка при загрузке QR-кода')
